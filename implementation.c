317fbf6d2d5b03eea68b641ddae84aa22045ae64
Alan Urmancheev <alan.urman@gmail.com>
2023-03-24 19:52:42 +0400

    Implementation: style

--- implementation.c
+++ implementation.c
@@ -3,7 +3,7 @@
 
 #define WRAP(NAME, TYPE) struct printer NAME(TYPE arg) { printf("%" #NAME, arg); return printer; }
 
-WRAP(s, char *)
+WRAP(s, char *);
 WRAP(d, int);
 WRAP(c, char);
 WRAP(f, float);
